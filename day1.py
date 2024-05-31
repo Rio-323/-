05647e6ca2f7a0a19576146dd978f8c39331cb8b
Rio-323 <kimsiyeon5214@gmail.com>
2024-05-31 12:01:04 +0900

    변수 선언과 자료형

---
+++ day1.py
@@ -0,0 +1,20 @@
+a = 3
+b = 2
+
+print(a + b)
+print(a * b)
+print(a - b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+c = 'Sparta'
+print(c)
+
+d = True
+print(d)
+
+e = (3 > 2)
+print(e)
+
